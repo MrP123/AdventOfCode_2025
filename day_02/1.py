72473a6dd209594161aa4d10e2b891907121863e
Matthias Panny <mrp.97.3@gmail.com>
2025-12-08 21:07:52 +0100

    Day 2 parts 1 & 2

---
+++ day_02/1.py
@@ -0,0 +1,26 @@
+import re
+
+ID_GROUPS = r"(\d*)-(\d*)" # extract start and end of ID range
+REPEATING_DIGITS = r"\b(\d+)(\1+)\b" # find repeating digits in ID --> group 1 and 2 could be unequal --> verify later
+
+id_ranges = []
+with open("./day_02/input.txt") as file:
+    id_ranges = [line.split(",") for line in file]
+    id_ranges = id_ranges[0] # should check
+
+
+invalid_id_sum = 0
+
+for id_range in id_ranges:
+    match = re.match(ID_GROUPS, id_range).groups()
+    if match:
+        range_start, range_end = map(int, match)
+        print(f"Range: {range_start}-{range_end}:")
+
+        for i in range(range_start, range_end + 1):
+            match = re.match(REPEATING_DIGITS, str(i))
+            if match and match.groups()[0] == match.groups()[1]:
+                invalid_id_sum += i
+                print(f"  Invalid ID found: {i}")
+
+print(invalid_id_sum)
