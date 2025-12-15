45a7eb5a56171964dfd747ea90541020de0ef22e
Matthias Panny <mrp.97.3@gmail.com>
2025-12-15 20:52:00 +0100

    Add day 4 part 1

---
+++ day_04/1.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+with open("./day_04/input.txt") as file:
+    grid = file.read().splitlines()
+
+mapping = {".": 0, "@": 1, "x": 2}
+inverse_map = {v: k for k, v in mapping.items()}
+
+grid = np.array([[mapping[char] for char in line] for line in grid])
+
+
+def get_neighbors(grid, x, y):
+    neighbors = []
+    for dy in [-1, 0, 1]:
+        for dx in [-1, 0, 1]:
+            if dy == 0 and dx == 0:
+                continue
+
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < grid.shape[0] and 0 <= ny < grid.shape[1]:
+                neighbors.append(grid[nx, ny])
+
+    return neighbors
+
+new_grid = np.copy(grid)
+amt_accessable = 0
+
+for y in range(grid.shape[0]):
+    for x in range(grid.shape[1]):
+        sum_neighbors = np.sum(get_neighbors(grid, x, y))
+
+        if grid[x, y] == 1 and sum_neighbors < 4:
+            new_grid[x, y] = 2
+            amt_accessable += 1
+
+
+new_grid = np.vectorize(inverse_map.get)(new_grid)
+print(new_grid)
+
+print(amt_accessable)
