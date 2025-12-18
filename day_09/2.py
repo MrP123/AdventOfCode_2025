d16591d3c543084aab3a7789368066a5917e6672
Matthias Panny <mrp.97.3@gmail.com>
2025-12-18 07:41:16 +0100

    Added day 9 part 2

---
+++ day_09/2.py
@@ -0,0 +1,26 @@
+from itertools import combinations
+import numpy as np
+from shapely import Polygon
+
+with open("./day_09/input.txt") as file:
+    red_tiles = file.read().splitlines()
+
+red_tiles = np.asarray([np.asarray(red_tile.split(","), dtype=int) for red_tile in red_tiles])
+
+full_polygon = Polygon(red_tiles)
+
+max_area = 0
+
+for (x1, y1), (x2, y2) in combinations(red_tiles, 2):
+
+    test_polygon = Polygon([(x1, y1), (x1, y2), (x2, y2), (x2, y1)])
+    
+    if full_polygon.contains(test_polygon):
+        dx = np.abs(x2 - x1) + 1
+        dy = np.abs(y2 - y1) + 1
+        area = dx * dy
+
+        if area > max_area:
+            max_area = area
+
+print(max_area)
